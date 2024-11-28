64c0a437af32c171875593c81c3f7ce649dbf269
Lai Dinh Cao Duc <ducldche176120@fpt.edu.vn>
2024-11-28 09:37:36 +0700

    feat: complete git hooks and react router v7 setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
-import type { Route } from '../+types/root';
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconBrandMantine } from '@tabler/icons-react';
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: 'New React Router App' },
     { name: 'description', content: 'Welcome to React Router!' },
@@ -8,12 +10,35 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
   return (
-    <div>
-      <h1>Welcome to React Router!</h1>
-      <p>
-        Get started by editing the file at <code>app/routes/home.tsx</code>
-      </p>
-    </div>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding='md'
+    >
+      <AppShell.Header>
+        <Group h='100%' px='md'>
+          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom='sm' size='sm' />
+          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom='sm' size='sm' />
+          <IconBrandMantine size={30} />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p='md'>
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt='sm' animate={true} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
   );
 }
